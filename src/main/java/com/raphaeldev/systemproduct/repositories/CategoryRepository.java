1c8236db1df9f86273b9551acce3ec56b3b68927
Raphael-Mendess <raphael.hkiller@gmail.com>
2026-05-24 22:49:14 -0300

    Including many-to-many annotation with JoinTable

--- src/main/java/com/raphaeldev/systemproduct/repositories/CategoryRepository.java
+++ src/main/java/com/raphaeldev/systemproduct/repositories/CategoryRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.raphaeldev.systemproduct.entities.Category;
 
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 public interface CategoryRepository extends JpaRepository <Category,Long>{ //registrar como componente do Spring
 
 }
