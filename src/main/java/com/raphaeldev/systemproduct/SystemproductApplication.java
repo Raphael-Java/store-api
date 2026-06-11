dfa95f2c282396030ce9298e27ddfb71616240b0
Raphael-Mendess <raphael.hkiller@gmail.com>
2026-06-10 23:53:12 -0300

    Inserting the user into the UserResource class

--- src/main/java/com/raphaeldev/systemproduct/SystemproductApplication.java
+++ src/main/java/com/raphaeldev/systemproduct/SystemproductApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SystemproductApplication {
+public class SystemProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SystemproductApplication.class, args);
+		SpringApplication.run(SystemProductApplication.class, args);
 	}
 }
